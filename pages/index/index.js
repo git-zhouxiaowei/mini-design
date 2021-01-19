f01d0409b40cacc62d10d02b8474a68c69a9082a
git-zhouxiaowei <283922486@qq.com>
2021-01-19 17:08:59 +0800

    首页轮播图，案例列表，通知列表后端获取

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+var dateUtils = require('../../utils/dateUtils')
 Page({
 
     /**
@@ -43,85 +44,28 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        // wx.request({
-        //   url: 'https://www.baidu.com/s',
-        //   data:{wd:'Java'},
-        //   success:(res)=>{
-        //       console.log(res);
-        //   },
-        //   fail:(err)=>{
-        //     console.log(err);
-        //   }
-        // })
-        this.setData({
-            bannerList: [
-                {bannerImg: '/static/images/banner/2.jpg'},
-                {bannerImg: '/static/images/banner/3.jpg'},
-                {bannerImg: '/static/images/banner/4.jpg'}
-            ],
-            designList: [
-                {
-                    id: 1,
-                    designImg: '/static/images/t1.jpg',
-                    designName: 'T1'
-                }, {
-                    id: 2,
-                    designImg: '/static/images/t2.jpg',
-                    designName: 'T2'
-                }, {
-                    id: 3,
-                    designImg: '/static/images/t3.jpg',
-                    designName: 'T3'
-                }, {
-                    id: 4,
-                    designImg: '/static/images/t4.jpg',
-                    designName: 'T4'
-                }, {
-                    id: 5,
-                    designImg: '/static/images/t5.jpg',
-                    designName: 'T5'
-                }, {
-                    id: 6,
-                    designImg: '/static/images/t1.jpg',
-                    designName: 'T1'
-                }, {
-                    id: 7,
-                    designImg: '/static/images/t2.jpg',
-                    designName: 'T2'
-                }, {
-                    id: 8,
-                    designImg: '/static/images/t3.jpg',
-                    designName: 'T3'
-                }, {
-                    id: 9,
-                    designImg: '/static/images/t4.jpg',
-                    designName: 'T4'
-                }, {
-                    id: 10,
-                    designImg: '/static/images/t5.jpg',
-                    designName: 'T5'
+        wx.request({
+            url: 'http://localhost:8080/design/front/mini/initData',
+            data: {wd: 'Java'},
+            success: (res) => {
+                console.log(res);
+                let respData = res.data;
+                if (0 == respData.code) {
+                    let dataMap = respData.data;
+                    dataMap.noticeList.forEach((item) => {
+                        item.createTime = dateUtils.formatTime(item.createTime, 'YY-MM-DD')
+                    })
+                    this.setData({
+                        bannerInfoList: dataMap.bannerInfoList,
+                        caseInfoList: dataMap.caseInfoList,
+                        noticeList: dataMap.noticeList
+                    });
                 }
-            ],
-            newsList: [
-                {
-                    id: 1,
-                    title: '十四届五中全会表示，要全面实现现代化主义强国',
-                    date: '2020-12-07'
-                }, {
-                    id: 2,
-                    title: '世界设计大会于浙江杭州举行，届时会有各方设计人员汇聚',
-                    date: '2020-12-01'
-                }, {
-                    id: 3,
-                    title: '关于设计的一些小技巧，你知道吗,设计方案的选定，创新力的发掘',
-                    date: '2020-11-17'
-                }, {
-                    id: 4,
-                    title: '网页设计方案的选定，创新力的发掘',
-                    date: '2020-11-10'
-                }
-            ]
-        });
+            },
+            fail: (err) => {
+                console.log(err);
+            }
+        })
     },
 
     /**
@@ -135,7 +79,6 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
     },
 
     /**
